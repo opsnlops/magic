bb1ed44f2f04058c04cc967c191cbc356a0adc41
Bunny Mickley <bunny@bunnynet.org>
2021-01-18 22:39:23 +0000

    Pass the raw data via the filter

--- smooth_data.py
+++ smooth_data.py
@@ -5,10 +5,50 @@
 
 import csv
 
+from scipy.signal import savgol_filter
+
+servo_lower_bound = 0
+servo_upper_bound = 179
+
 data_file = 'data/sample_raw.csv'
 
+raw_data = []
+
+def remove_comments(csvfile):
+    for row in csvfile:
+        raw = row.split('#')[0].strip()
+        if raw: yield row
+
+def smooth_data(raw_data, window_length=None, polyorder=None):
+    window_length = window_length or 11
+    polyorder = polyorder or 3
+
+    finished_data = []
+
+    smoothed_data = savgol_filter(raw_data, window_length, polyorder)
+    for point in smoothed_data:
+
+        # Make sure we're in range of the servo
+        int_point = round(point)
+
+        if int_point < servo_lower_bound:
+            int_point = servo_lower_bound
+        elif int_point > servo_upper_bound:
+            int_point = servo_upper_bound
+
+        finished_data.append(int_point)
+    
+    return finished_data
+
 with open(data_file) as raw:
-    csv_reader = csv.reader(raw)
+    csv_reader = csv.reader(remove_comments(raw))
 
     for row in csv_reader:
-        print(row)
+        raw_data.append(int(row[0]))
+
+print(raw_data)
+
+smoothed_data = smooth_data(raw_data)
+
+for point in smoothed_data:
+    print(point)
